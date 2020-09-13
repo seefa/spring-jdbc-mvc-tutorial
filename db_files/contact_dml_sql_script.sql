ba247f3192caae369159c4f877719f61fb5ebf23
Saman Delfani <sami.delfan@gmail.com>
2020-09-13 22:58:28 +0430

    add database DDL and DML files

---
+++ db_files/contact_dml_sql_script.sql
@@ -0,0 +1,2 @@
+INSERT INTO MyDB.contact (contact_id, name, family, birthday, phone, telephone, email, address) VALUES (1, 'Saman', 'Delfani', '1987-08-26', '09137390432', null, null, null);
+INSERT INTO MyDB.contact (contact_id, name, family, birthday, phone, telephone, email, address) VALUES (2, 'Hamed', 'Delfani', '1990-09-15', '02188453089', null, null, null);
